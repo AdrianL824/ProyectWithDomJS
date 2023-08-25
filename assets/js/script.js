b656c812270517d0d4f2383ff358393339bcd2d8
AdrianL824 <adrian.lopez.or2408@gmail.com>
2023-08-25 14:18:34 -0400

    Revert "Clase 05 completada"
    This reverts commit 218a48d8345dfdd7d7d6e9aa345e759fa9c27a75.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,56 @@
-import checkComplete from "../../components/checkComplete.js";
-import deleteIcon from "../../components/deleteIcon.js";
-(() => {
-    const btn = document.querySelector('[data-form-btn]');
+( () => {
+const btn = document.querySelector('[data-form-btn]');
 
-    const createTask = (event) => {
-        event.preventDefault();
-        const input = document.querySelector('[data-form-input]');
-        const value = input.value
-        const list = document.querySelector('[data-list]');
-        const task = document.createElement('li');
-        task.classList.add('card');
-        input.value = '';
-        //backticks alt gr + }
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = '';
+    //backticks alt gr + }
+    
+    //Creados un contenedor de html con JS
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    
+    const content = `
 
-        //Creados un contenedor de html con JS
-        const taskContent = document.createElement("div");
-        taskContent.appendChild(checkComplete());
-        const titleTask = document.createElement("span");
-        titleTask.classList.add("task");
-        titleTask.innerText = value;
-        taskContent.appendChild(titleTask);
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
+    list.appendChild(task);
+}
 
-        //task.innerHTML = content;
-        task.appendChild(taskContent);
-        task.appendChild(deleteIcon());
-        list.appendChild(task);
-    }
+btn.addEventListener("click", createTask);
 
-    btn.addEventListener("click", createTask);
+const checkComplete = () => {
+    const i = document.createElement('i');
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask)
+    return i;
+}
+
+const completeTask = () =>{
+    const element = event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+}
+
 })()
 
+// const trash = () = > {
+//     <i class="fas fa-trash-alt trashIcon icon"></i>`;
+//     const e = document.createElement("i");
+//     return e;
 
+// }
 
 //------------------------------------------------
 //ideas - pruebas
